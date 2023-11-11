ea36605f503c32b033db7ffd590183c5bc152e7d
Craig Craig <craig@craig.se>
2023-11-11 17:35:29 -0600

    Custom Version.cs

--- AppInfo.cs
+++ AppInfo.cs
@@ -1,8 +1,10 @@
 ﻿namespace CheeseyUI;
 
+using CheeseyUtils;
+
 public static class AppInfo
 {
     public static string Name { get; set; } = string.Empty;
     public static string Author { get; set; } = string.Empty;
-    public static CheeseyUtils.Version Version { get; set; } = new(0, 0, 0, 0);
+    public static Version Version { get; set; } = new(0, 0, 0, 0);
 }
