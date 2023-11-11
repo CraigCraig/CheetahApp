222c38b37ca7ed0ef96aabd482ab82ce37ee188d
Craig Craig <craig@craig.se>
2023-11-11 17:08:45 -0600

    Moved input class to its own file

--- App.cs
+++ App.cs
@@ -1,6 +1,5 @@
 ﻿namespace CheeseyUI;
 
-using System;
 using SFML.Graphics;
 using SFML.System;
 using SFML.Window;
@@ -54,37 +53,6 @@ public class App
             RootElement.Draw(_window);
 
             _window.Display();
-        }
-    }
-}
-
-public static class Input
-{
-    public static EventHandler<MouseButtonEventArgs> OnMouseClicked { get; } = (sender, args) => { };
-    public static Vector2i MousePosition { get; internal set; }
-
-    internal static void HandleMouseClick(MouseButtonEventArgs args)
-    {
-        if (App.Instance is null) return;
-
-        lock (App.Instance.RootElement.Children)
-        {
-            foreach (var element in App.Instance.RootElement.Children)
-            {
-                if (element.IsActive)
-                {
-                    if (element.IsMouseOver())
-                    {
-                        if (element is Button button)
-                        {
-                            button.HandleClick();
-                            break;
-                        }
-                    }
-                }
-            }
-
-            OnMouseClicked.Invoke(null, args);
         }
     }
 }
---
+++ Input.cs
@@ -0,0 +1,36 @@
+﻿namespace CheeseyUI;
+
+using System;
+using SFML.System;
+using SFML.Window;
+
+public static class Input
+{
+    public static EventHandler<MouseButtonEventArgs> OnMouseClicked { get; } = (sender, args) => { };
+    public static Vector2i MousePosition { get; internal set; }
+
+    internal static void HandleMouseClick(MouseButtonEventArgs args)
+    {
+        if (App.Instance is null) return;
+
+        lock (App.Instance.RootElement.Children)
+        {
+            foreach (var element in App.Instance.RootElement.Children)
+            {
+                if (element.IsActive)
+                {
+                    if (element.IsMouseOver())
+                    {
+                        if (element is Button button)
+                        {
+                            button.HandleClick();
+                            break;
+                        }
+                    }
+                }
+            }
+
+            OnMouseClicked.Invoke(null, args);
+        }
+    }
+}
